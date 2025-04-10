6224e2bdc966083d6fe2f0484446a29b36cfa273
bigluckykush <josephzamani861@gmail.com>
2025-04-10 03:17:52 -0700

    second commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,46 @@
+ "use client"
+import { useEffect, useState } from "react"
 
 
-export default function Home() {
-  return (
-    <main>landing page</main>
-  );
+export default function home () {
+    const [count,setCount] = useState(0) 
+    return (
+        <main classsName="flax flex-col gap-20 items-center justify-center px-20 py-20">
+            <p className="text-4xl font-bold">count: {count}</p>
+            <button onClick={() => setCount(count + 1 )} className="w-[200px] h-[50px] text-white bg-blue-400">Increment</button>
+            <button onClick={() => setCount(count - 1 )} className="w-[200px] h-[50px] text-white bg-blue-400">Decrement</button>
+        </main>
+
+    )
 }
+
+
+
+
+
+
+
+
+// import Image from "next/image"
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <div>
+//         <p>My Image</p>
+//         <Image width={300} height={300} src="/2.jpg" alt="image" />
+//       </div>
+//     </main>
+          
+//   )
+// }
+
+
+// useEffect (() => {
+//     const getDate = async () => {
+//         const respones = await fetch ("http://dummyjson.com/products")
+//         const data = await respones.json()
+//         console.log(data.products)
+//     }
+//     getDate()
+//  })
